5014a937c5730b127cb0a92fb3a0f8e038e94829
mon6996 <thanaporn_chan@cmu.ac.th>
2021-11-26 15:48:47 +0700

    complete lab6_1

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+int main()
+{
+    int num = 1, even = 0, odd = 0;
+    while(num != 0)
+    {
+        cout << "Enter an integer: ";
+        cin >> num;
+        if(num != 0)
+        {
+            if(num%2 == 0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+    }
+
+    cout << "#Even numbers = "  << even << endl;
+    cout << "#Odd numbers = " << odd;
     return 0;
 }
